dbdb523a8b2d625344c414d0dd9c92069ea0a0db
Damien Bevins <DBevins@student.neumont.edu>
2018-02-23 19:03:43 -0700

    Added the data I've altered to the Account and Campaign classes

--- RPG-Manager/RPG-Manager/Areas/RPGArea/Models/Account.cs
+++ RPG-Manager/RPG-Manager/Areas/RPGArea/Models/Account.cs
@@ -7,5 +7,11 @@ namespace RPG_Manager.Areas.RPGArea.Models
 {
     public class Account
     {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public object Image { get; set; }
+        public ICollection<Character> Characters { get; set; }
+        public string Email { get; set; }
     }
 }
--- RPG-Manager/RPG-Manager/Areas/RPGArea/Models/Campaign.cs
+++ RPG-Manager/RPG-Manager/Areas/RPGArea/Models/Campaign.cs
@@ -7,5 +7,10 @@ namespace RPG_Manager.Areas.RPGArea.Models
 {
     public class Campaign
     {
+        public int Campaign_ID { get; set; }
+        public Account DungeonMaster { get; set; }
+        public Dictionary<Account, Character> Characters { get; set; }
+        public Dictionary<Account, string> Log { get; set; }
+        public int Turns { get; set; }
     }
 }
